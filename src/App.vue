1e6940327898887197db3a6cef363583ea6e30e0
andrewthesloth <svecond3@fel.cvut.cz>
2023-09-18 12:48:49 +0200

    idk

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+// @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
 
 html, body {
   margin: 0;
